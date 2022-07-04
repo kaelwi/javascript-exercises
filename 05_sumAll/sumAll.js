4f8c4a12b39d00ebed3d06909965296f6e6a6cdb
Karoline Wild <karoline.elisabeth.wild@seznam.cz>
2022-07-04 14:48:41 +0200

    Exercise 05_sumall solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(from, to) {
+    if (typeof(from) != typeof(to)) {
+        return 'ERROR';
+    }
+    if (from < 0 || to < 0) {
+        return 'ERROR';
+    }
 
+    let sum = 0;
+    let temp;
+
+    if (from > to) {
+        let temp = to;
+        to = from;
+        from = temp;
+    }
+
+    while (from <= to) {
+        sum += from;
+        from++;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
