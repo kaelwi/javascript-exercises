7c163645a7f984cbcfb1cc4fce7fa407ac7ba2b0
Karoline Wild <karoline.elisabeth.wild@seznam.cz>
2022-07-06 12:03:02 +0200

    Exercise 08_calculator solution

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,56 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	if (array.length == 0) {
+    return 0;
+  }
+
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+
+  return parseInt(sum);
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  if (array.lenght == 0) {
+    return 0;
+  }
 
+  let res = 1;
+  for (let i = 0; i < array.length; i++) {
+    res *= array[i];
+  }
+
+  return res;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  if (b == 0) {
+    return 1;
+  }
+
+	let res = 1;
+  for (let i = 0; i < b; i++) {
+    res *= a;
+  }
+
+  return res;
 };
 
-const factorial = function() {
+const factorial = function(a) {
+  if (a == 0 || a == 1) {
+    return 1;
+  }
+
+  return a * factorial(a - 1);
 	
 };
 
