d0093dcfd52d4a0e4b2bfacf13108fafe9c64d5c
Karoline Wild <karoline.elisabeth.wild@seznam.cz>
2022-07-06 12:16:46 +0200

    Exercise 09_palindrome solution

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (word) {
 
+    word = word.replace(/[^A-Za-z]/g, '');
+    word = word.toLowerCase();
+
+    let half = Math.ceil(word.length / 2);
+
+    for (let i = 0; i <= half; i++) {
+        if (word.charAt(i) !== word.charAt(word.length-i-1)) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
