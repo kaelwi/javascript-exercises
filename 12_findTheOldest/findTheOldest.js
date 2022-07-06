ab9f93e79eb9c32d5c97b0026fb6008a318567b1
Karoline Wild <karoline.elisabeth.wild@seznam.cz>
2022-07-06 13:27:50 +0200

    Exercise 12_findTheOldest solution

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,9 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldest, current) => {
+        let oldestAge = oldest.yearOfDeath ? oldest.yearOfDeath - oldest.yearOfBirth : new Date().getFullYear() - oldest.yearOfBirth;
+        let currentAge =  current.yearOfDeath ? current.yearOfDeath - current.yearOfBirth : new Date().getFullYear() - current.yearOfBirth;
+        return oldestAge > currentAge ? oldest : current;
+    })
 };
 
 // Do not edit below this line
